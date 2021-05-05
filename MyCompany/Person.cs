223dd7db3a8ff52eeb28e025cacb3ce4520e1fa8
Giannos-G <giannos.gavrielides@gmail.com>
2021-05-05 19:29:00 +0300

    Add project files.

---
+++ MyCompany/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCompany
+{
+    public class Person
+    {
+        //Properties
+        public string Name { get; set; } // gia na mporo na ta kalo me tin telia "."
+
+        public void SetName (string _name)
+        {
+            name = _name;
+        }
+
+        public void GetName()
+        {
+            return name;
+        }
+    }
+}
---
+++ MyCompany/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;   //namespace
+
+namespace MyCompany  // build your own namespace
+{                   // curly bracket 
+    class Program           // ti tha exei mesa i klasi  
+    {                       
+        static void Main(string[] args)         //method signature, anafora tis klasis, panta proti
+        {
+            Person person = new Person
+            {
+                Name = "Giannos"        // an eixa alla variables diaxorizo me komma
+            };
+            person.Name="Giannos";
+            Console.WriteLine(person.Name);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
